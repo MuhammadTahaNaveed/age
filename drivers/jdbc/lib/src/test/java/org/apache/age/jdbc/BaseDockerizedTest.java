3463924580ae2018acb8964204833ea12d79b6b3
MuhammadTahaNaveed <mnaveed.bese20seecs@seecs.edu.pk>
2023-03-21 09:50:47 +0500

    Update CIs to pull age image from corresponding pg version tag

--- drivers/jdbc/lib/src/test/java/org/apache/age/jdbc/BaseDockerizedTest.java
+++ drivers/jdbc/lib/src/test/java/org/apache/age/jdbc/BaseDockerizedTest.java
@@ -51,7 +51,7 @@ public class BaseDockerizedTest {
     public void beforeAll() throws Exception {
         String CORRECT_DB_PASSWORDS = "postgres";
         agensGraphContainer = new GenericContainer<>(DockerImageName
-            .parse("apache/age:latest"))
+            .parse("apache/age:PG11_latest"))
             .withEnv("POSTGRES_PASSWORD", CORRECT_DB_PASSWORDS)
             .withExposedPorts(5432);
         agensGraphContainer.start();
